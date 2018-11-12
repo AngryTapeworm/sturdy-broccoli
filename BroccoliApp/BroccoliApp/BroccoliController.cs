a64a66f5ab25e0238fe3f5d6131167bb1efd0622
Alzbeta <alzbetadavidova@seznam.cz>
2018-11-12 19:56:05 +0100

    Commit property

--- BroccoliApp/BroccoliApp/BroccoliController.cs
+++ BroccoliApp/BroccoliApp/BroccoliController.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BroccoliApp
+﻿namespace BroccoliApp
 {
     class BroccoliController
     {
+        public BroccoliType Type { get; set; }
     }
 
     public enum BroccoliType
